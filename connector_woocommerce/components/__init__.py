a5a34109c7b0527053b492d6b84d10e9b1b55956
Antonyraj Jacob <antonyraj.j.serpentcs@gmail.com>
2018-05-02 19:30:40 +0530

    [ADD] connector_woocommerce: Added license

--- connector_woocommerce/components/__init__.py
+++ connector_woocommerce/components/__init__.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+# © 2009 Tech-Receptives Solutions Pvt. Ltd.
+# © 2018 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # See LICENSE file for full copyright and licensing details.
 
 from . import core
--- connector_woocommerce/components/core.py
+++ connector_woocommerce/components/core.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
-# See LICENSE file for full copyright and licensing details.
+# © 2009 Tech-Receptives Solutions Pvt. Ltd.
+# © 2018 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.addons.component.core import AbstractComponent
 
